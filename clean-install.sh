c1edc6358cb11e9b9b2b606c1c32612a435a173b
Brandon Fisher <brandon@brandonfisher.dev>
2022-01-29 22:20:03 -0600

    Update install script

--- clean-install.sh
+++ clean-install.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-#-----LOAD PROGRESS-----
-if [ -e /home/dotfiles_install.progress ] ; then
+#-----LOAD PROGRESS-----------
+if [ -e ~/dotfiles_install.progress ] ; then
   echo "Checking Dotfiles Install Progress..."
-  apt_pkgs_installed=$(cat /home/dotfiles_install.progress | grep DOTFILES_APT_PKGS_INSTALLED | wc -l)
+  apt_pkgs_installed=$(cat ~/dotfiles_install.progress | grep DOTFILES_APT_PKGS_INSTALLED | wc -l)
+  ssh_key_ready=$(cat ~/dotfiles_install.progress | grep DOTFILES_SSH_KEY_READY | wc -l)
 else
   echo "Initializing Dotfiles Install"
   apt_pkgs_installed=0
+  ssh_key_ready=0
 fi
+#-----------------------------
 
+#-----SETUP SSH KEY-----------
+if [ $ssh_key_ready -eq 0 ] ; then
+  read -p "SSH Key Email: " ssh_email
+  ssh-keygen -t ed25519 -C $ssh_email
+  eval "$(ssh-agent -s)"
+  ssh-agent add /home/brandon/.ssh/id_ed25519
+  if [ $? -eq 0 ] ; then
+    echo "DOTFILES_SSH_KEY_READY" >> ~/dotfiles_install.progress
+  else
+    echo "Error adding ssh key to ssh-agent"
+  fi
+  cat ~/.ssh/id_ed25519.pub | /mnt/c/Windows/System32/clip.exe
+  echo "SSH Key Ready. id_ed25519.pub copied to clipboard"
+fi
+#-----------------------------
 
-#-----APT PACKAGES-----
-
+#-----APT PACKAGES------------
 if [ $apt_pkgs_installed -eq 0 ] ; then
   echo "Installing apt packages..."
   sudo apt install -y git sshpass
 
   #DOTFILES_APT_PKGS_INSTALLED=1
   if [ $? -eq 0 ] ; then
-    echo "DOTFILES_APT_PKGS_INSTALLED" >> /home/dotfiles_install.progress
+    echo "DOTFILES_APT_PKGS_INSTALLED" >> ~/dotfiles_install.progress
   else
     echo "Error loading apt packages"
   fi
 else
   echo "Apt packages loaded"
 fi
+#-----------------------------
+
+#-----GIT SETUP / SSH---------
+
+#-----------------------------
 
 
-
-#-----APT PACKAGES-----
 #-----APT PACKAGES-----
 #-----APT PACKAGES-----
