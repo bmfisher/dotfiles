e3adfd5c0f801de0673cf3e541ba9d5244ac3eb0
Brandon Fisher <brandon@brandonfisher.dev>
2022-03-31 22:11:16 -0500

    Starting init.lua

---
+++ sample-init.lua
@@ -0,0 +1,20 @@
+-- Migrating vimrc to init.lua
+--
+
+vim.opt.errorbells = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.nu = true
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = '~/.vim/undodir'
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.relativenumber = true
+vim.opt.hidden = true
+vim.opt.guicursor = ''
